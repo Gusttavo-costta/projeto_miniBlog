bbc77c7264af1bb4df70f64c2a77ff079e3f83df
Gusttavo-costta <gccostta23@gmail.com>
2023-11-09 17:38:05 -0300

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8uOj1yzU1KH-l1vA7XMrW8FRGESirCrk",
+  authDomain: "miniblog-93162.firebaseapp.com",
+  projectId: "miniblog-93162",
+  storageBucket: "miniblog-93162.appspot.com",
+  messagingSenderId: "216006540553",
+  appId: "1:216006540553:web:ba61554248c5aee000a7d8"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+//ccs
+import Styles from "./Home.module.css"
+
+const Home = () => {
+  return (
+    <div>
+        <h1>home</h1>
+    </div>
+  )
+}
+
+export default Home
