c261d62c517d83d8242806aa0ef12ecfa42050b2
Gusttavo-costta <gccostta23@gmail.com>
2023-11-23 17:36:51 -0300

    :construction:

---
+++ src/hooks/useAuthentication.js
@@ -0,0 +1,26 @@
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassowrd,
+    updateProfile,
+    signOut
+} from 'firebase/auth'
+
+import { useState, useEffect } from 'react'
+
+export const useAuthentication = () => {
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
+
+    //cleanup
+    // deal with memory leak (lidar com vazamento de memória)
+    const [cancelled,setCancelled] = useState(false)
+
+    const auth = getAuth()
+
+    function checkIfIsCancelled() {
+        if (cancelled) {
+            return;
+        }
+    }
+};
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,9 @@
+import styles from "./Login.module.css";
+
+function Login() {
+    return (
+        <div><h2>Login</h2></div>
+    )
+}
+
+export default Login;
